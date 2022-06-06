7660111ef37915cf36784d1aab54a38858cbb091
Hail-cali <willnotproud5@gmail.com>
2022-06-06 13:31:19 +0900

    Modified : major update for exp scenario scrip ======================================
    Train in Server, deploy to client opt
    
    `deploy_cls`  when deploy model, opt to use only classifier or whole network
    
    `freeze_cls` Freeze classifier network, train backbone, aux, global layer
    
    `global_loss` use aux loss for global federated learning

--- exp.sh
+++ exp.sh
@@ -1,1 +1,167 @@
-python main_hail --
+
+# 26 new -----------------
+# False global loss
+python main_hail.py --readme 'HAIL.freeze_cls.use_net' \
+      --num_clients 3   --freeze_cls --aux_loss  \
+      --start_epoch 0  --epochs 5 --gpu 1 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_off_gls_fre' \
+      --resume '/home/hail09/FedPn/experiments/coco_off_gls_fre'
+
+# use global loss
+python main_hail.py --readme 'HAIL.freeze_cls.use_net' \
+      --num_clients 3  --global_loss --freeze_cls --aux_loss  \
+      --start_epoch 0  --epochs 5 --gpu 3 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_on_gls_fre' \
+      --resume '/home/hail09/FedPn/experiments/coco_on_gls_fre'
+
+      #   -----  use pretrained cls only,
+  # FAsle global loss
+  python main_hail.py --readme 'HAIL.freeze_cls.use_cls' \
+        --num_clients 3   --freeze_cls --aux_loss   --deploy_cls \
+        --start_epoch 0  --epochs 5 --gpu 1 \
+        --cfg_path './experiments/coco.personalized.config.json' \
+        --save_root '/home/hail09/FedPn/experiments/coco_off_gls_fre' \
+        --resume '/home/hail09/FedPn/experiments/coco_off_gls_fre'
+
+  # use global loss
+  python main_hail.py --readme 'HAIL.freeze_cls.use_cls' \
+        --num_clients 3  --global_loss --freeze_cls --aux_loss  --deploy_cls  \
+        --start_epoch 0  --epochs 5 --gpu 3 \
+        --cfg_path './experiments/coco.personalized.config.json' \
+        --save_root '/home/hail09/FedPn/experiments/coco_on_gls_fre' \
+        --resume '/home/hail09/FedPn/experiments/coco_on_gls_fre'
+
+
+
+
+
+# ----------------- 26 new end
+
+
+
+
+
+
+# 153 new -----------------
+# False global loss, use net
+python main_hail.py --readme 'HAIL.freeze_cls.use_net' \
+      --num_clients 3   --freeze_cls --aux_loss  \
+      --start_epoch 0  --epochs 5 --gpu 0 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_off_fre_net' \
+      --resume '/home/hail09/FedPn/experiments/coco_off_fre_net'
+
+
+# use global loss use net
+python main_hail.py --readme 'HAIL.freeze_cls.use_net' \
+      --num_clients 3  --global_loss --freeze_cls --aux_loss  \
+      --start_epoch 0  --epochs 5 --gpu 1 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_on_fre_net' \
+      --resume '/home/hail09/FedPn/experiments/coco_on_fre_net'
+
+
+          #   -----  use pretrained cls only,
+      # FAsle global loss
+      python main_hail.py --readme 'HAIL.freeze_cls.use_cls' \
+            --num_clients 3   --freeze_cls --aux_loss   --deploy_cls \
+            --start_epoch 0  --epochs 5 --gpu 1 \
+            --cfg_path './experiments/coco.personalized.config.json' \
+            --save_root '/home/hail09/FedPn/experiments/coco_off_fre_cls' \
+            --resume '/home/hail09/FedPn/experiments/coco_off_fre_cls'
+
+      # use global loss
+      python main_hail.py --readme 'HAIL.freeze_cls.use_cls' \
+            --num_clients 3  --global_loss --freeze_cls --aux_loss  --deploy_cls  \
+            --start_epoch 0  --epochs 5 --gpu 0 \
+            --cfg_path './experiments/coco.personalized.config.json' \
+            --save_root '/home/hail09/FedPn/experiments/coco_on_fre_cls' \
+            --resume '/home/hail09/FedPn/experiments/coco_on_fre_cls'
+
+
+
+# 153
+## clinet setting 3 off global_loss
+python main_hail.py --num_clients 5 --epochs 5 --gpu 1  --global_loss 0  --start_epoch 0 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_off_gls_5' \
+      --resume '/home/hail09/FedPn/experiments/coco_off_gls_5' \
+
+
+
+
+python main_hail.py --num_clients 4 --epochs 5 --gpu 0  --global_loss 1  --start_epoch 0 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_on_global_4' \
+      --resume '/home/hail09/FedPn/experiments/coco_on_global_4'\
+
+
+python main_hail_sec.py --num_clients 3 --epochs 5 --gpu 0  --global_loss 1  --start_epoch 0 \
+      --freeze_cls 1 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_freeze_global_3' \
+      --resume '/home/hail09/FedPn/experiments/coco_freeze_global_3'\
+
+
+
+
+
+
+
+#26
+python main_hail.py --num_clients 3 --epochs 5 --gpu 3  --global_loss 0  --start_epoch 0 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_off_gls' \
+      --resume '/home/hail09/FedPn/experiments/coco_off_gls' \
+      --readme ''
+
+
+python main_hail.py --num_clients 3 --epochs 5 --gpu 1  --global_loss 1  --start_epoch 0  \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_on_global' \
+      --resume '/home/hail09/FedPn/experiments/coco_on_global'
+
+
+#26
+python main_hail.py --num_clients 3 --epochs 6 --gpu 3  --global_loss 0  --start_epoch 3 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_off_gls_new' \
+      --resume '/home/hail09/FedPn/experiments/coco_off_gls_new'
+
+
+python main_hail.py --num_clients 3 --epochs 6 --gpu 1  --global_loss 1  --start_epoch 3  \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_on_global_new' \
+      --resume '/home/hail09/FedPn/experiments/coco_on_global_new'
+
+
+# 26 - freeze classifier, train aux, global, pretrain whole net
+python main_hail.py --num_clients 3 --epochs 5 --gpu 3  --global_loss 1  --start_epoch 0 \
+      --cfg_path './experiments/coco.personalized.config.json' \
+      --save_root '/home/hail09/FedPn/experiments/coco_freeze_cls' \
+      --resume '/home/hail09/FedPn/experiments/coco_freeze_cls'
+
+
+
+
+
+
+
+# on gloabl_loss  single
+python main_hail.py --single_mode 1 --gpu 0 --global_loss 1 --start_epoch 0  \
+     --save_root '/home/hail09/FedPn/experiments/coco_on_single' \
+     --resume '/home/hail09/FedPn/experiments/coco_on_single'
+
+# off gloabl_loss single
+python main_hail.py --single_mode 1 --gpu 3 --global_loss 0 --start_epoch 0 \
+     --save_root '/home/hail09/FedPn/experiments/coco_off_single' \
+     --resume '/home/hail09/FedPn/experiments/coco_off_single'
+
+# python main_hail.py --single_mode 1 --gpu 0 --global_loss 0 --cfg_path './experiments/coco.personalized.config.json'
+
+
+
+
+#
--- opt.py
+++ opt.py
@@ -12,11 +12,14 @@ model's params is there
 def parse_opt():
     parser = argparse.ArgumentParser(prog='setting API')
 
+    parser.add_argument('--readme', default='', type=str)
+
     # base setting (model, basenet, mode, pretrained)
     parser.add_argument('--mode', default='train', help='train | inference | test ')
+    parser.add_argument('--single_mode', default=0, type=bool, help='when training on single site, use this opt in sh')
 
     parser.add_argument('--basenet', default='mobilenet', help='pretraiend base model')
-    parser.add_argument('--model_name', default='mpn', help='mpn | fpn')
+    parser.add_argument('--model_name', default='fedpn', help='mpn | fpn')
     parser.add_argument('--pretrained', default=False, type=bool)
 
 
@@ -28,15 +31,23 @@ def parse_opt():
     parser.add_argument('--rank', default=3)
     parser.add_argument('--world-size', default=2, type=int,
                         help='number of distributed processes')
-    parser.add_argument('--dist-url', default='file://home/hail09/FedPn/distributed_file_sys/some.txt', help='url used to set up distributed training env://')
+    parser.add_argument('--dist-url', default='', help='url used to set up distributed training env://')
 
     parser.add_argument('--device', default='cpu', help='allocated in api')
 
+    parser.add_argument('--aux_loss', default=False,  action='store_true')
+    parser.add_argument('--global_loss', default=False,  action='store_true')
+    parser.add_argument('--freeze_cls', default=False,  action='store_true')
+    parser.add_argument('--deploy_cls', default=False, action='store_true',
+                        help='if True(input),use finetuned cls layer | default False , use whole finetune ')
+
     # file root
     parser.add_argument('--root', default='/home/hail09/FedPn')
-    parser.add_argument('--model_dir', default='./experiments', help='temp')
-    parser.add_argument('--cfg_path', default='./experiments/baseline.config.json')
-    parser.add_argument('--save_root', default='/home/hail09/FedPn/experiments/coco_mobile',
+    parser.add_argument('--model_dir', default='./experiments', help='exp dir in root dir')
+    parser.add_argument('--cfg_path', default='./experiments/coco.personalized.config.json',
+                        help='baseline.config.json | coco.personalized.config.json')
+
+    parser.add_argument('--save_root', default='/home/hail09/FedPn/experiments/test_sample',
                         help='Location to save checkpoint models')
 
 
@@ -47,29 +58,31 @@ def parse_opt():
     parser.add_argument('--train_sets', default=['train2017'])
     parser.add_argument('--val_sets', default=['val2017'])
 
-    # parser.add_argument('--input_dim', default=600, type=int, help='Input Size for SSD')
 
     parser.add_argument('--train_size', default=0.8)
 
     parser.add_argument('--epochs', default=3, type=int)
     parser.add_argument('--start_epoch', default=0, type=int)
-    parser.add_argument('--aux_loss', default=True, type=bool)
-    parser.add_argument('--resume', default='',
+    parser.add_argument('--resume', default='/home/hail09/FedPn/experiments/test_sample',
                         help='resume checkpoint path | /home/hail09/FedPn/experiments/coco_mobile')
     parser.add_argument('--test_only', default=False, type=bool, help='if True, test mode, False, load state_dict')
 
     # log
     parser.add_argument('--tensorboard', default=True)
-    parser.add_argument('--log_interval', default=50, type=int, help='Log interval for showing training loss')
+    parser.add_argument('--log_interval', default=100, type=int, help='Log interval for showing training loss')
+    parser.add_argument('--max_log', default=20, type=int)
+    # server Initialized
+    parser.add_argument('--initial_cls', default='g_net_classifier.pth', type=str)
+    parser.add_argument('--cls_path', default='', type=str)
 
     # client_setting
     parser.add_argument('--client_type', default='client_all', type=str,
-                        help = 'client_obj | client_animal | client_vehicle | client_all')
+                        help = 'client_obj | client_animal | client_vehicle | client_all') # not used
 
-    parser.add_argument('--num_clients', default=3)
+    parser.add_argument('--num_clients', default=3, type=int)
     parser.add_argument('--update_status', default=False, help='is needed to update params from cluster')
     parser.add_argument('--global_model_path', default='global.pth')
-    parser.add_argument('--global_gpu_set', default=True)
+    parser.add_argument('--global_gpu_set', default=True, type=bool)
 
     parser.add_argument('--verbose', default=False)
 
